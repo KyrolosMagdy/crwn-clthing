0c41521e0c42cb426a5f563fb1cea96f504f8748
unknown <pero.kero01@gmail.com>
2019-07-21 22:49:31 +0200

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price })=> {
+	/*because stripe deals with money in cents USD currency looks like egyptian 2ersh*/
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_jOY6RSrKWYxIzhWCu0YLn2e900KfhgcaiJ' ;
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+	}
+
+	return (
+		<StripeCheckout 
+			label=" Pay Now "
+			name="Crwn Clothing"
+			billingAddress
+			shippingAdress
+
+			description= {`Your total is ${price}`}
+			amount = {priceForStripe}
+			panelLabel = 'Pay Now'
+			token = {onToken}
+			stripeKey = { publishableKey }
+		/>
+	);
+};
+
+export default StripeCheckoutButton ;
