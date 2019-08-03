746d3a067b40cdaa4696542e54ca214079e43532
unknown <pero.kero01@gmail.com>
2019-08-03 04:18:36 +0200

    removed redux thunk and used redux saga intead to handle our side-effects functions

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import { compose } from 'redux';
+
+import { selectCollectionFetching } from '../../redux/shop/shop.selectors';
+import withSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+	isLoading: selectCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+	connect(mapStateToProps),
+	withSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
-
 import rootReducer from './root-reducer';
+import createSagaMiddleware from 'redux-saga';
 
-const middlewares = [];
+import { fetchCollectionsStart } from './shop/shop.sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger)
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(fetchCollectionsStart)
+
 const persistor = persistStore( store );
 
 export { store, persistor } ;
